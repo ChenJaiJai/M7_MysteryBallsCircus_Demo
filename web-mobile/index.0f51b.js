fa6831430dc73e04933d010c61566bf1b2a94777
JaiJai <wm.rd01@gmail.com>
2025-04-25 16:10:32 +0800

    自適應畫面版本改回

--- web-mobile/index.0f51b.js
+++ web-mobile/index.0f51b.js
@@ -1,4 +1,4 @@
-System.register(["./application.5e197.js"], function (_export, _context) {
+System.register(["./application.047ea.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
