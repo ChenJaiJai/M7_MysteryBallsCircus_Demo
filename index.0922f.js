92bdd1830263b1da89050c98581d275d141f7b5b
JaiJai <wm.rd01@gmail.com>
2025-04-25 15:55:19 +0800

    更新飛過高的錯誤

--- index.0922f.js
+++ index.0922f.js
@@ -1,4 +1,4 @@
-System.register(["./application.a6e0e.js"], function (_export, _context) {
+System.register(["./application.f8647.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
